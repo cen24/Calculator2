584a59427e50cb3f048901107cadf10a4325272c
Chinedu Nnaji <cen24@njit.edu>
2019-12-09 02:55:16 -0500

    Feature: code for customer data class

---
+++ Database/create_model.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, Integer, String, Column, DateTime, Numeric, ForeignKey, SmallInteger
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+
+engine = create_engine('sqlite:////web/Sqlite-Data/example.db')
+
+# this loads the sqlalchemy base class
+Base = declarative_base()
+
+# Setting up the classes that create the record objects and define the schema
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    address = Column(String(200), nullable=False)
+    town = Column(String(200), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    orders = relationship("Order", backref='customer')
